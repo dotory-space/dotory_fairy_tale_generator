4007abddd36744d14d0f5a3917a3217b491e2392
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-08 14:41:02 +0900

    feat:log

--- dotory_fairy_tale_generator/model.py
+++ dotory_fairy_tale_generator/model.py
@@ -2,9 +2,13 @@ import torch
 from transformers import GPT2LMHeadModel, GPT2Config
 
 def get_model(checkpoint_path, config_file_path):
+    print('[fairy-tale-generator] [get-model] [1]')
     model = GPT2LMHeadModel(config=GPT2Config.from_json_file(config_file_path))
+    print('[fairy-tale-generator] [get-model] [2]')
     checkpoint = torch.load(checkpoint_path)
+    print('[fairy-tale-generator] [get-model] [3]')
     model.load_state_dict(checkpoint)
+    print('[fairy-tale-generator] [get-model] [4]')
 
     return model
 
--- dotory_fairy_tale_generator/tokenizer.py
+++ dotory_fairy_tale_generator/tokenizer.py
@@ -1,7 +1,8 @@
 from transformers import PreTrainedTokenizerFast
 
 def get_tokenizer():
+    print('[fairy-tale-generator] [get_tokenizer] [1]')
     tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2', bos_token='</s>', eos_token='</s>', unk_token='<unk>', pad_token='<pad>', mask_token='<mask>')
-
+    print('[fairy-tale-generator] [get_tokenizer] [1]')
     return tokenizer
     
