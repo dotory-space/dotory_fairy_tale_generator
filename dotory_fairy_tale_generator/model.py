a8fc01925b717f68cef5691785ba599bd5c7e444
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-08 14:30:56 +0900

    feat:device 제거

--- dotory_fairy_tale_generator/model.py
+++ dotory_fairy_tale_generator/model.py
@@ -1,14 +1,10 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Config
 
-def get_model(checkpoint_path, config_file_path, device):
+def get_model(checkpoint_path, config_file_path):
     model = GPT2LMHeadModel(config=GPT2Config.from_json_file(config_file_path))
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint)
-
-    model = GPT2LMHeadModel(GPT2Config.from_json_file(config_path))
-    model.load_state_dict(checkpoint['model'])
-    model.to(device)
 
     return model
 
