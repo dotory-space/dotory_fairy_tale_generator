035776b44f7cd9f24ed8cacedb2f4f8b9fc8f06f
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-08 14:43:04 +0900

    feat:log 추가

--- dotory_fairy_tale_generator/model.py
+++ dotory_fairy_tale_generator/model.py
@@ -3,12 +3,14 @@ from transformers import GPT2LMHeadModel, GPT2Config
 
 def get_model(checkpoint_path, config_file_path):
     print('[fairy-tale-generator] [get-model] [1]')
-    model = GPT2LMHeadModel(config=GPT2Config.from_json_file(config_file_path))
+    config = GPT2Config.from_json_file(config_file_path)
     print('[fairy-tale-generator] [get-model] [2]')
-    checkpoint = torch.load(checkpoint_path)
+    model = GPT2LMHeadModel(config=config)
     print('[fairy-tale-generator] [get-model] [3]')
-    model.load_state_dict(checkpoint)
+    checkpoint = torch.load(checkpoint_path)
     print('[fairy-tale-generator] [get-model] [4]')
+    model.load_state_dict(checkpoint)
+    print('[fairy-tale-generator] [get-model] [5]')
 
     return model
 
