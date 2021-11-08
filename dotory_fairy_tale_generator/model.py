40e7facb912433f4a36ce84bdd65e4f2701f3838
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-08 14:58:17 +0900

    feat:model 변경

--- dotory_fairy_tale_generator/model.py
+++ dotory_fairy_tale_generator/model.py
@@ -5,7 +5,7 @@ def get_model(checkpoint_path, config_file_path):
     print('[fairy-tale-generator] [get-model] [1]')
     config = GPT2Config.from_json_file(config_file_path)
     print('[fairy-tale-generator] [get-model] [2]')
-    model = GPT2LMHeadModel(config=config)
+    model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
     print('[fairy-tale-generator] [get-model] [3]')
     checkpoint = torch.load(checkpoint_path)
     print('[fairy-tale-generator] [get-model] [4]')
