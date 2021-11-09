6975821fbc117d57aaf2586915b1212e9467e5a7
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-09 09:31:20 +0900

    feat:tokenizer log 삭제

--- dotory_fairy_tale_generator/tokenizer.py
+++ dotory_fairy_tale_generator/tokenizer.py
@@ -1,8 +1,6 @@
 from transformers import PreTrainedTokenizerFast
 
 def get_tokenizer():
-    print('[fairy-tale-generator] [get_tokenizer] [1]')
     tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2', bos_token='</s>', eos_token='</s>', unk_token='<unk>', pad_token='<pad>', mask_token='<mask>')
-    print('[fairy-tale-generator] [get_tokenizer] [1]')
     return tokenizer
     
