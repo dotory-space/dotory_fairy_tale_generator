9795ad755a1ff1a207f42a52df041d39f0d41728
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-09 09:19:12 +0900

    feat:device 추가

---
+++ dotory_fairy_tale_generator/device.py
@@ -0,0 +1,16 @@
+import torch
+
+def get_device():
+    if torch.cuda.is_available():
+        device_count = torch.cuda.device_count()
+        print("device_count: {}".format(device_count))
+        for device_num in range(device_count):
+            print("device {} capability {}".format(
+                device_num,
+                torch.cuda.get_device_capability(device_num)))
+            print("device {} name {}".format(
+                device_num, torch.cuda.get_device_name(device_num)))
+    else:
+        print("no cuda device")
+
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- dotory_fairy_tale_generator/model.py
+++ dotory_fairy_tale_generator/model.py
@@ -1,8 +1,7 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Config
 
-def get_model(checkpoint_path, config_file_path):
-    device = torch.device('cpu')
+def get_model(checkpoint_path, config_file_path, device):
     config = GPT2Config.from_json_file(config_file_path)
     model = GPT2LMHeadModel(config=config)
     checkpoint = torch.load(checkpoint_path, map_location=device)
