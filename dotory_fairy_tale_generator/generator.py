509e2dc830adcb217efaf0bf3e807dfd6fec8703
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-01-05 12:28:51 +0900

    feat:log 제거

--- dotory_fairy_tale_generator/generator.py
+++ dotory_fairy_tale_generator/generator.py
@@ -26,6 +26,7 @@ class FairyTaleGenerator:
         f = open(filtering_file_path, 'r')
         self.filtering = f.read().split('\n')
         f.close()
+        print('[FTG] filtering loaded: ', self.filtering)
 
     def translate_kakao(self, text, source, target):
         url = "https://translate.kakao.com/translator/translate.json"
@@ -51,7 +52,6 @@ class FairyTaleGenerator:
         encoded = torch.LongTensor().to(self.device)
 
         encoded = torch.cat([encoded, torch.tensor(self.tokenizer.encode(input_sentence)).to(self.device)])
-        print('Length of encoded:', len(encoded))
         generated = self.model.generate(encoded.unsqueeze(0), do_sample=True, use_cache=True, top_p=0.9, num_return_sequences=3, max_length=len(encoded)+100, min_length=len(encoded), temperature=0.6, pad_token_id=self.tokenizer.eos_token_id).to(self.device)  # length_penalty=10,
         generated = [generated[i][len(encoded):] for i in range(3)]  # input 중복 제거
         decoded = [self.tokenizer.decode(generated[i]) for i in range(3)]  # decode
@@ -67,15 +67,5 @@ class FairyTaleGenerator:
                 except:output_eng[i] = decoded
                 output_kor[i] = self.translate_kakao(output_eng[i], 'en', 'ku')
                 output_kor[i] = spell_checker.check(output_kor[i]).checked
-            print('Generated ' + str(i+1) + ' : ', output_kor[i])
-
-        # input_sentence = self.translate_kakao(input_sentence, 'kr', 'en')  # papago : ko, kakao : kr
-        # encoded = torch.tensor(self.tokenizer.encode(input_sentence)).unsqueeze(0).to(self.device)
-        # generated = self.model.generate(encoded, do_sample=True, top_p=0.9, num_return_sequences=3, max_length=200, min_length=1, temperature=0.6, pad_token_id=self.tokenizer.eos_token_id).to(self.device)  # length_penalty=10, 
-        # decoded = [self.tokenizer.decode(generated[i]) for i in range(3)]  # decode
-        # output_eng = [decoded[i].replace(input_sentence, '').lstrip(' ') for i in range(3)]  # input 중복 문장 제거
-        # output_eng = [sent_tokenize(output_eng[i])[0] for i in range(3)]  # 첫 번째 문장 분리
-        # output_kor = [self.translate_kakao(output_eng[i], 'en', 'ku') for i in range(3)]  # papgo : ko, kakao : ku (ku : 높임말 문체 in kakao)
-        # output_kor = [spell_checker.check(output_kor[i]).checked for i in range(3)]  # 맞춤법 검사
 
         return output_kor
